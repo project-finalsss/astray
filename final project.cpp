a1487d0dfdb0702d3a8c2c4aa4f8614fada5b367
YourSupport <52892442+YourSupport@users.noreply.github.com>
2019-07-14 13:26:35 -0700

    Update final project.cpp

--- final project.cpp
+++ final project.cpp
@@ -5,6 +5,8 @@
 #include <iomanip>
 #include <cmath>
 #include <string>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 void opening();
@@ -21,7 +23,8 @@ void opening()
     cin.get();
     cout << "It is the year 2525. Many people have lost their memories due to the Great Mystery. Tell me child, do you remember who you are?\n" ;
     cin.get();
-    cout << "Who am I? (Enter your name)\n";
+    cout << "Who am I?\n";
+    cin.get();
     choice(1);
     cin.get();
     cin.ignore();
@@ -41,6 +44,63 @@ void opening()
 
 void choice(int j)
 {
+    if (j = 1)
+    {
+        cout << "1. I don’t know.\n"
+    	     << "2. I don’t know.\n"
+	         << "3. I don’t know.\n"
+	         << "4. I don’t know.\n";
+    }
+}
+
+
+
+/*NPC
+
+farmerNPC();
+    int option,
+        health = 5,
+        attack = 1,
+        armor = 1,
+        mgc_resist = 0;
+    cout << "Hey there! I'm the farmer! Would you like to buy my beets?"
+    //for loop until farmer dies??
+    option = combatmenu(); //open combat menu (returns their option)
+    if (option = 1) //(physical attack) 
+        health = health - (plyr_atk - armor);
+        cout << "OW DAG NABBIT. YUR GONNA PAY!";
+    else if (option = 2) //(magical attack)
+        health = health - (plyr_mgc - mgc_resist);
+        cout << "WHAT ARE YOU? A MAGICIAN?";
+    else if (option = 3) //(talk)
+        cout << "You will? Thank you! My family eatin' good t'night.";
+        cout << "Here, I have an extra tractor. You can have it for free!";
+        attack++;
+        help_counter++;
+        return help_counter, attack;
+    else //(option = 4) run 
+        cout << "MY BEETS! BUY THEM PLEASEEE.";
+        cout << "<You got away safely.>"
+        run_counter++;
+        return run_counter;
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+*/
     if (j = 1)
     {
         cout << "1. I don’t know.\n"
